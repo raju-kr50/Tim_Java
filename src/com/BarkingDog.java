7aa2121044e03d11ec2d353d4e216bad07c49e6a
Raju Kumar <you@example.comraju_kr50@yahoo.com>
2021-03-16 14:22:57 +0530

    Barking Dogs

---
+++ src/com/BarkingDog.java
@@ -0,0 +1,22 @@
+package com;
+
+public class BarkingDog {
+
+    public static void main(String[] args) {
+        System.out.println(shouldWakeUp(true, 1));
+        System.out.println(shouldWakeUp(false, 2));
+        System.out.println(shouldWakeUp(true, 8));
+        System.out.println(shouldWakeUp(true, -1));
+    }
+    public static boolean shouldWakeUp(boolean barking, int hourOfDay) {
+        if (hourOfDay >= 0 && hourOfDay < 24) {
+            if (barking && (hourOfDay < 8 || hourOfDay > 22)) {
+                return true;
+            } else
+                return false;
+        } else
+            return false;
+    }
+
+}
+
