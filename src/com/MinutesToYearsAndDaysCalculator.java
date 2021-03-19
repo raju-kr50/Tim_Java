032849e976b6174741e6a0d6495ea18359cae10d
Raju Kumar <you@example.comraju_kr50@yahoo.com>
2021-03-19 08:34:22 +0530

    Changed Indentation

--- src/com/MinutesToYearsAndDaysCalculator.java
+++ src/com/MinutesToYearsAndDaysCalculator.java
@@ -8,15 +8,14 @@ public class MinutesToYearsAndDaysCalculator {
         printYearsAndDays(561600);
     }
 
-    public static void printYearsAndDays(long minutes){
-        if (minutes<0){
+    public static void printYearsAndDays(long minutes) {
+        if (minutes < 0) {
             System.out.println("Invalid Value");
-        } else
-        {
-            long days = (minutes/(60*24));
-            long years = days/365;
-            long remainingDays = days%365;
-            System.out.println(minutes+" min = "+years+" y and "+remainingDays+" d");
+        } else {
+            long days = (minutes / (60 * 24));
+            long years = days / 365;
+            long remainingDays = days % 365;
+            System.out.println(minutes + " min = " + years + " y and " + remainingDays + " d");
         }
     }
 }
