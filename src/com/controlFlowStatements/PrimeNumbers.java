b77db92d8f9c21d4c76fc6c6b97c0c8ae04c8251
Raju Kumar <you@example.comraju_kr50@yahoo.com>
2021-03-19 23:13:26 +0530

    Add PrimeNumbers

--- src/com/controlFlowStatements/PrimeNumbers.java
+++ src/com/controlFlowStatements/PrimeNumbers.java
@@ -3,24 +3,25 @@ package com.controlFlowStatements;
 public class PrimeNumbers {
 
     public static void main(String[] args) {
-        int numberOfPrime =0;
-        for (int i=100; i<=1000; i++){
-            if (isPrime(i)){
-                System.out.println(i+" is a Prime Number");
+        int numberOfPrime = 0;
+        for (int i = 100; i <= 1000; i++) {
+            if (isPrime(i)) {
+                System.out.println(i + " is a Prime Number");
                 numberOfPrime++;
-                if (numberOfPrime==3){
+                if (numberOfPrime == 3) {
                     System.out.println("Exceeding for loop...");
                     break;
                 }
             }
         }
     }
-    public static boolean isPrime(int number){
-        if (number==1){
+
+    public static boolean isPrime(int number) {
+        if (number == 1) {
             return false;
         }
-        for (int i =2; i<=number/2; i++){
-            if (number%i==0){
+        for (int i = 2; i <= number / 2; i++) {
+            if (number % i == 0) {
                 return false;
             }
         }
