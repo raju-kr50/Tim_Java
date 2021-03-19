b69978809c3efb2a14d552bf19e11f41c05357e2
Raju Kumar <you@example.comraju_kr50@yahoo.com>
2021-03-19 23:13:05 +0530

    Add CalculateInterest

---
+++ src/com/controlFlowStatements/PrimeNumbers.java
@@ -0,0 +1,29 @@
+package com.controlFlowStatements;
+
+public class PrimeNumbers {
+
+    public static void main(String[] args) {
+        int numberOfPrime =0;
+        for (int i=100; i<=1000; i++){
+            if (isPrime(i)){
+                System.out.println(i+" is a Prime Number");
+                numberOfPrime++;
+                if (numberOfPrime==3){
+                    System.out.println("Exceeding for loop...");
+                    break;
+                }
+            }
+        }
+    }
+    public static boolean isPrime(int number){
+        if (number==1){
+            return false;
+        }
+        for (int i =2; i<=number/2; i++){
+            if (number%i==0){
+                return false;
+            }
+        }
+        return true;
+    }
+}
