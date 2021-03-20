714ef2652e581b733331f4d8df248b023ebff3b7
Raju Kumar <you@example.comraju_kr50@yahoo.com>
2021-03-20 23:57:57 +0530

    Added EvenDigitSum

---
+++ src/com/controlFlowStatements/EvenDigitSum.java
@@ -0,0 +1,26 @@
+package com.controlFlowStatements;
+
+public class EvenDigitSum {
+
+    public static void main(String[] args) {
+        System.out.println(getEvenDigitSum(123456789));
+        System.out.println(getEvenDigitSum(252));
+        System.out.println(getEvenDigitSum(-22));
+    }
+
+    public static int getEvenDigitSum(int number) {
+        if (number < 0) {
+            return -1;
+        }
+        int digit = 0;
+        int sum = 0;
+        while (number > 0) {
+            digit = number % 10;
+            number /= 10;
+            if (digit % 2 == 0) {
+                sum += digit;
+            }
+        }
+        return sum;
+    }
+}
