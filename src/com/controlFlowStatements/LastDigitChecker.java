abdeafd4f967429c235f4cf4fad4c1cebeeacd56
Raju Kumar <you@example.comraju_kr50@yahoo.com>
2021-03-21 01:12:41 +0530

    Added LastDigitChecker

---
+++ src/com/controlFlowStatements/LastDigitChecker.java
@@ -0,0 +1,28 @@
+package com.controlFlowStatements;
+
+public class LastDigitChecker {
+
+    public static void main(String[] args) {
+        System.out.println(hasSameLastDigit(41, 22, 71));
+        System.out.println(hasSameLastDigit(23, 32, 42));
+        System.out.println(hasSameLastDigit(9, 99, 999));
+
+    }
+
+    public static boolean hasSameLastDigit(int num1, int num2, int num3) {
+        if (isValid(num1) && isValid(num2) && isValid(num3)) {
+            if (num1 % 10 == num2 % 10 || num2 % 10 == num3 % 10 || num1 % 10 == num3 % 10) {
+                return true;
+            } else
+                return false;
+        } else
+            return false;
+    }
+
+    public static boolean isValid(int number) {
+        if (number >= 10 && number <= 1000) {
+            return true;
+        } else
+            return false;
+    }
+}
