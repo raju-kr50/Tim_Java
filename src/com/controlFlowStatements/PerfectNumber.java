36a2dc7e71fc6041590ab82b192a436eda3d411d
Raju Kumar <you@example.comraju_kr50@yahoo.com>
2021-03-21 11:51:24 +0530

    Added PerfectNumber

---
+++ src/com/controlFlowStatements/PerfectNumber.java
@@ -0,0 +1,28 @@
+package com.controlFlowStatements;
+
+public class PerfectNumber {
+
+    public static void main(String[] args) {
+        System.out.println(isPerfectNumber(6));
+        System.out.println(isPerfectNumber(28));
+        System.out.println(isPerfectNumber(5));
+        System.out.println(isPerfectNumber(-1));
+    }
+
+    public static boolean isPerfectNumber(int number) {
+        if (number < 1) {
+            return false;
+        }
+        int sum = 0;
+        for (int i = 1; i <= number; i++) {
+            if (number % i == 0) {
+                sum = sum + i;
+            }
+        }
+        sum -= number;
+        if (sum == number) {
+            return true;
+        } else
+            return false;
+    }
+}
