9ee1eb9d0b271a7c518cc0bb5ef8fc3efbf428b0
Raju Kumar <you@example.comraju_kr50@yahoo.com>
2021-03-23 23:37:21 +0530

    Added Cylinder

--- src/com/oops/inheritance/Main.java
+++ src/com/oops/inheritance/Main.java
@@ -8,5 +8,17 @@ public class Main {
         ford.accelerate(30);
         ford.accelerate(15);
         ford.accelerate(-20);
+
+        Circle circle = new Circle(3.75);
+        System.out.println("circle.radius = " + circle.getRadius());
+        System.out.println("circle.area = " + circle.getArea());
+
+        Cylinder cylinder = new Cylinder(5.55, 7.25);
+        System.out.println("cylinder.radius = " + cylinder.getRadius());
+        System.out.println("cylinder.height = " + cylinder.getHeight());
+        System.out.println("cylinder.area = " + cylinder.getArea());
+        System.out.println("cylinder.volume = " + cylinder.getVolume());
+
+
     }
 }
