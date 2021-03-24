f8a4c023ad5ef79d5127b607a33e721849bf3812
Raju Kumar <you@example.comraju_kr50@yahoo.com>
2021-03-24 11:25:13 +0530

    Added Cuboid

---
+++ src/com/oops/inheritance/Cuboid.java
@@ -0,0 +1,23 @@
+package com.oops.inheritance;
+
+public class Cuboid extends Rectangle {
+
+    private double height;
+
+    public Cuboid(double length, double width, double height) {
+        super(length, width);
+        if (height < 0) {
+            this.height = 0;
+        } else {
+            this.height = height;
+        }
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    public double getVolume() {
+        return getArea() * height;
+    }
+}
