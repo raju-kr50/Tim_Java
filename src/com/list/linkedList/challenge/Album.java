2e78ee7e384a7d1b86ec4522355a99a294dc25b1
raju-kr50 <R@ju.kr50>
2021-04-11 00:52:08 +0530

    Added Playlist Example

---
+++ src/com/list/linkedList/challenge/Album.java
@@ -0,0 +1,56 @@
+package com.list.linkedList.challenge;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+public class Album {
+
+    private String name;
+    private String artist;
+    private ArrayList<Song> songs;
+
+    public Album(String name, String artist) {
+        this.name = name;
+        this.artist = artist;
+        this.songs = new ArrayList<>();
+    }
+
+    public boolean addSong(String title, double duration) {
+        Song song = findSong(title);
+        if (song != null) {
+            return false;
+        }
+        this.songs.add(new Song(title, duration));
+        return true;
+    }
+
+    private Song findSong(String title) {
+        for (Song checkedSong : this.songs) {
+            if (checkedSong.getTitle().equals(title)) {
+                return checkedSong;
+            }
+        }
+        return null;
+    }
+
+    public boolean addToPlayList(String title, LinkedList<Song> playlist) {
+        Song song = findSong(title);
+        if (song != null) {
+            playlist.add(song);
+            return true;
+        }
+        System.out.println("The song " + title + " is not in this album");
+        return false;
+    }
+
+    public boolean addToPlayList(int trackNumber, LinkedList<Song> playlist) {
+        int index = trackNumber - 1;
+        if ((index >= 0) && (index <= this.songs.size())) {
+            playlist.add(this.songs.get(index));
+            return true;
+        }
+        System.out.println("This album does not have a track" + trackNumber);
+        return false;
+    }
+}
